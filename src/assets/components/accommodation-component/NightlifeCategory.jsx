8b571546f1aa4d69d9f853c1b227a0fd5cbe6118
azeezfasasi <fas.azeez@gmail.com>
2025-08-21 15:54:16 +0100

    updated night life category

--- src/assets/components/accommodation-component/NightlifeCategory.jsx
+++ src/assets/components/accommodation-component/NightlifeCategory.jsx
@@ -3,25 +3,53 @@ import { Link } from 'react-router-dom';
 import finediningimg from '../../images/finediningimg.svg'
 import buffetimg from '../../images/buffetimg.svg'
 import delicaciesimg from '../../images/delicaciesimg.svg'
+import games from '../../images/games.svg'
+import latenightdining from '../../images/latenightdining.svg'
+import liveentertainment from '../../images/liveentertainment.svg'
+import luxury from '../../images/luxury.svg'
 
 // Main component that contains all the cards
 export default function NightLifeCategory() {
   const categories = [
     {
-      title: 'BARS & PUBS',
+      title: 'BARS &amp; LOUNGES',
       image: finediningimg,
       alt: 'A classy restaurant interior with hanging lights and a bar.',
       link: '/category/nightlife',
     },
     {
-      title: 'CLUBS & LOUNGES',
+      title: 'OUTDOOR NIGHTLIFE',
       image: buffetimg,
       alt: 'A bustling scene of people enjoying a buffet.',
       link: '/category/nightlife',
     },
     {
-      title: 'LIVE MUSIC & ENTERTAINMENT',
+      title: 'CLUBBING &amp RESERVATIONS',
       image: delicaciesimg,
+      alt: 'A vibrant disco ball with two cocktail glasses in the foreground.',
+      link: '/category/nightlife',
+    },
+    {
+      title: 'GAMES &am ALTERNATIVE NIGHTS',
+      image: games,
+      alt: 'A vibrant disco ball with two cocktail glasses in the foreground.',
+      link: '/category/nightlife',
+    },
+    {
+      title: 'LATE NIGHT DINING',
+      image: latenightdining,
+      alt: 'A vibrant disco ball with two cocktail glasses in the foreground.',
+      link: '/category/nightlife',
+    },
+    {
+      title: 'LIVE ENTERTAINMENT',
+      image: liveentertainment,
+      alt: 'A vibrant disco ball with two cocktail glasses in the foreground.',
+      link: '/category/nightlife',
+    },
+    {
+      title: 'Luxury &amp Chills Night',
+      image: luxury,
       alt: 'A vibrant disco ball with two cocktail glasses in the foreground.',
       link: '/category/nightlife',
     },
@@ -30,7 +58,7 @@ export default function NightLifeCategory() {
   return (
     <div className="bg-white flex items-center justify-center p-4 sm:p-8">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:flex-row items-center justify-center space-y-6 lg:space-y-0 lg:space-x-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-4 space-y-6 lg:space-y-0 lg:space-x-6">
           {categories.map((category, index) => (
             <Link
               key={index}
